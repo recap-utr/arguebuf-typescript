d407f6b9117ada8ef58cb7c5c296c315ca4e9fd9
Leon Kamke <64018777+leonkamke@users.noreply.github.com>
2023-01-30 20:25:30 +0100

    feat: add fromProtobuf converter (#11)
    * Added "fromProtobuf" feature and some tests (2)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/converter/protobuf.ts
+++ src/converter/protobuf.ts
@@ -1,8 +1,10 @@
 import * as protobuf from "arg-services/graph/v1/graph_pb";
 import { Edge } from "../model/edge.js";
-import { Node } from "../model/node.js";
+import { AtomNode, Node, SchemeNode } from "../model/node.js";
 import { Graph } from "../model/graph.js";
+import { Metadata } from "../model/metadata.js";
 import * as date from "../services/date.js";
+import { Analyst, Participant, Resource } from "../index.js";
 
 export function edgeToProtobuf(edge: Edge): protobuf.Edge {
   return new protobuf.Edge({
@@ -108,5 +110,141 @@ export function toProtobuf(graph: Graph): protobuf.Graph {
     userdata: graph.userdata,
     participants: participants,
     resources: resources,
+  });
+}
+
+export function edgeFromProtobuf(
+  id: string,
+  obj: protobuf.Edge,
+  nodes: { [key: string]: Node }
+): Edge {
+  return new Edge({
+    id: id,
+    source: nodes[obj.source],
+    target: nodes[obj.target],
+    metadata: new Metadata(),
+  });
+}
+
+export function nodeFromProtobuf(id: string, obj: protobuf.Node): Node {
+  const timestamp = date.now();
+  const metadata = new Metadata({
+    created:
+      obj.metadata?.created === undefined
+        ? timestamp
+        : date.fromProtobuf(obj.metadata?.created),
+    updated:
+      obj.metadata?.updated === undefined
+        ? timestamp
+        : date.fromProtobuf(obj.metadata?.updated),
+  });
+  if (obj.type.case === "atom") {
+    return new AtomNode({
+      id: id,
+      text: obj.type.value.text,
+      metadata: metadata,
+      userdata: obj.userdata,
+    });
+  } else {
+    return new SchemeNode({
+      id: id,
+      metadata: metadata,
+      userdata: obj.userdata,
+      premise_descriptors: obj.type.value?.premiseDescriptors,
+      scheme: obj.type.value?.type,
+    });
+  }
+}
+
+export function fromProtobuf(obj: protobuf.Graph): Graph {
+  const timestamp = date.now();
+  const nodes = Object.fromEntries(
+    Object.entries(obj.nodes).map(([key, value]) => [
+      key,
+      nodeFromProtobuf(key, value),
+    ])
+  );
+  const edges = Object.fromEntries(
+    Object.entries(obj.edges).map(([key, value]) => [
+      edgeFromProtobuf(key, value, nodes),
+    ])
+  );
+  const analysts: { [key: string]: Analyst } = Object.fromEntries(
+    Object.entries(obj.analysts).map(([key, value]) => [
+      key,
+      new Analyst({
+        id: key,
+        email: value.email,
+        name: value.name,
+        userdata: value.userdata,
+      }),
+    ])
+  );
+  const resources: { [key: string]: Resource } = Object.fromEntries(
+    Object.entries(obj.resources).map(([key, value]) => [
+      key,
+      new Resource({
+        text: value.text,
+        id: key,
+        source: value.source,
+        metadata: new Metadata({
+          created:
+            value.metadata?.created === undefined
+              ? timestamp
+              : date.fromProtobuf(value.metadata?.created),
+          updated:
+            value.metadata?.updated === undefined
+              ? timestamp
+              : date.fromProtobuf(value.metadata?.updated),
+        }),
+        timestamp: date.fromProtobuf(value.timestamp),
+        title: value.title,
+        userdata: value.userdata,
+      }),
+    ])
+  );
+  const participants: { [key: string]: Participant } = Object.fromEntries(
+    Object.entries(obj.participants).map(([key, value]) => [
+      key,
+      new Participant({
+        id: key,
+        description: value.description,
+        email: value.email,
+        location: value.location,
+        name: value.name,
+        url: value.url,
+        username: value.username,
+        metadata: new Metadata({
+          created:
+            value.metadata?.created === undefined
+              ? timestamp
+              : date.fromProtobuf(value.metadata?.created),
+          updated:
+            value.metadata?.updated === undefined
+              ? timestamp
+              : date.fromProtobuf(value.metadata?.updated),
+        }),
+        userdata: value.userdata,
+      }),
+    ])
+  );
+  return new Graph({
+    nodes: nodes,
+    edges: edges,
+    analysts: analysts,
+    majorClaim: obj.majorClaim,
+    userdata: obj.userdata,
+    resources: resources,
+    metadata: new Metadata({
+      created:
+        obj.metadata?.created === undefined
+          ? timestamp
+          : date.fromProtobuf(obj.metadata?.created),
+      updated:
+        obj.metadata?.updated === undefined
+          ? timestamp
+          : date.fromProtobuf(obj.metadata?.updated),
+    }),
+    participants: participants,
   });
 }
