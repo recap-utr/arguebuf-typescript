ed1ffacd365e55f34dc951e10c95403dac2de791
leonkamke <l.kamke@web.de>
2023-01-30 19:29:48 +0100

    Added "fromProtobuf" feature and some tests (2)

--- src/test/protobuf.test.ts
+++ src/test/protobuf.test.ts
@@ -6,13 +6,28 @@ import * as date from "../services/date.js";
 
 const timestamp = date.now();
 
-// Test atom node
+/*
+  Test atom node
+*/
 const modelAtomNode: model.AtomNode = new model.AtomNode({
   id: "119935",
   text: "One can hardly move in Friedrichshain or Neukölln these days without permanently scanning the ground for dog dirt.",
   metadata: new model.Metadata({
     created: timestamp,
     updated: timestamp,
+  }),
+});
+
+const protobufAtomNode: protobufModel.Node = new protobufModel.Node({
+  type: {
+    case: "atom",
+    value: new protobufModel.Atom({
+      text: "One can hardly move in Friedrichshain or Neukölln these days without permanently scanning the ground for dog dirt.",
+    })
+  },
+  metadata: new protobufModel.Metadata({
+    created: date.toProtobuf(timestamp),
+    updated: date.toProtobuf(timestamp),
   }),
 });
 
@@ -31,7 +46,24 @@ test("atom node: model2protobuf", () => {
   }
 });
 
-// Test scheme node
+test("atom node: protobuf2model", () => {
+  const modelNode: model.Node = protobuf.nodeFromProtobuf("119935", protobufAtomNode);
+  expect(modelNode.type).toBe("atom");
+  if (modelNode.type === "atom") {
+    assertType<model.Node>(modelNode);
+    expect(modelNode.text).toStrictEqual(
+      "One can hardly move in Friedrichshain or Neukölln these days without permanently scanning the ground for dog dirt."
+    );
+    expect(modelNode.metadata!.created).toEqual(timestamp);
+    expect(modelNode.metadata!.updated).toEqual(timestamp);
+    expect(modelNode.userdata).toMatchObject({});
+  }
+});
+
+
+/*
+  Test scheme node
+*/
 const modelSchemeNode: model.SchemeNode = new model.SchemeNode({
   id: "119935",
   scheme: {
@@ -41,6 +73,22 @@ const modelSchemeNode: model.SchemeNode = new model.SchemeNode({
   metadata: new model.Metadata({
     created: timestamp,
     updated: timestamp,
+  }),
+});
+
+const protobufSchemeNode: protobufModel.Node = new protobufModel.Node({
+  type: {
+    case: "scheme",
+    value: new protobufModel.Scheme({
+      type: {
+        case: "support",
+        value: protobufModel.Support.DEFAULT,
+      }
+    })
+  },
+  metadata: new protobufModel.Metadata({
+    created: date.toProtobuf(timestamp),
+    updated: date.toProtobuf(timestamp),
   }),
 });
 
@@ -58,5 +106,21 @@ test("scheme node: model2protobuf", () => {
     expect(protobufNode.metadata!.updated).toEqual(date.toProtobuf(timestamp));
     expect(protobufNode.userdata).toMatchObject({});
     expect(protobufNode.type.value.premiseDescriptors).toEqual([]);
+  }
+});
+
+test("scheme node: protobuf2model", () => {
+  const modelNode: model.Node = protobuf.nodeFromProtobuf("119935", protobufSchemeNode);
+  expect(modelNode.type).toBe("scheme");
+  if (modelNode.type === "scheme") {
+    assertType<model.Node>(modelNode);
+    expect(modelNode.scheme.case).toBe("support");
+    expect(modelNode.scheme.value).toBe(
+      protobufModel.Support.DEFAULT
+    );
+    expect(modelNode.metadata!.created).toEqual(timestamp);
+    expect(modelNode.metadata!.updated).toEqual(timestamp);
+    expect(modelNode.userdata).toMatchObject({});
+    expect(modelNode.premise_descriptors).toEqual([]);
   }
 });
