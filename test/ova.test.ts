405dd4f86ba752bb9c6ec3d075b18f9c261d9552
leonkamke <l.kamke@web.de>
2023-02-23 16:05:33 +0100

    Corrected location of ova test file

--- test/ova.test.ts
+++ test/ova.test.ts
@@ -1,6 +1,6 @@
 import { assertType, expect, test } from "vitest";
-import ovaGraph from "../../data/arguebase-private/recap/format=ova,lang=de/Hamburg/Positionspapiere/20100524_HH_Schulverbesserer_merge_20190228_0955.json";
-import * as arguebuf from "../index.js";
+import ovaGraph from "../data/arguebase-private/recap/format=ova,lang=de/Hamburg/Positionspapiere/20100524_HH_Schulverbesserer_merge_20190228_0955.json";
+import * as arguebuf from "../src/index.js";
 
 test("graph: ova2arguebuf", () => {
   const g = arguebuf.load.ova(
