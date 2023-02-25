9991fb68e462a3d55c5ccb77d02b6256bc720010
Mirko Lenz <mirko@mirkolenz.com>
2023-02-25 12:27:19 +0100

    fix: properly convert between protobuf/json

--- src/dump/json.ts
+++ src/dump/json.ts
@@ -2,13 +2,15 @@ import * as model from "../model/index.js";
 import { aif as dumpAif } from "./aif.js";
 import { protobuf as dumpProtobuf } from "./protobuf.js";
 
+type JSONObject = { [key: string]: any };
+
 export function json(
   graph: model.Graph,
   format: "aif" | "arguebuf"
-): { [key: string]: any } {
+): JSONObject {
   if (format === "aif") {
     return dumpAif(graph);
   }
 
-  return dumpProtobuf(graph);
+  return dumpProtobuf(graph).toJson() as JSONObject;
 }
--- src/load/json.ts
+++ src/load/json.ts
@@ -1,12 +1,15 @@
+import * as protoModel from "arg-services/graph/v1/graph_pb";
 import * as model from "../model/index.js";
 import * as schemas from "../schemas/index.js";
 import { aif as loadAif } from "./aif.js";
 import { protobuf as loadProtobuf } from "./protobuf.js";
 
-export function json(graph: { [key: string]: any }): model.Graph {
+type JSONObject = { [key: string]: any };
+
+export function json(graph: JSONObject): model.Graph {
   if (graph.locutions !== undefined) {
     return loadAif(graph as schemas.aif.Graph);
   }
 
-  return loadProtobuf(graph as schemas.protobuf.Graph);
+  return loadProtobuf(protoModel.Graph.fromJson(graph));
 }
