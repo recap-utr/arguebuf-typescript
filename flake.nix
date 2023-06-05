bc171074ad0c606a1b031d8a71fda41c9de31fbe
Mirko Lenz <mirko@mirkolenz.com>
2023-06-05 10:42:01 +0200

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          shellHook = "npm install";
+          packages = with pkgs; [nodejs-18_x];
+        };
+      };
+    };
+}
