9fb6b0fe7affcc3ac768b311812aec3e14c15f91
Mirko Lenz <mirko@mirkolenz.com>
2024-10-25 12:04:53 +0200

    chore: apply treefmt

--- src/model/utils.ts
+++ src/model/utils.ts
@@ -5,8 +5,8 @@ export type RecursivePartial<T> = {
   [P in keyof T]?: T[P] extends (infer U)[]
     ? RecursivePartial<U>[]
     : T[P] extends object
-    ? RecursivePartial<T[P]>
-    : T[P];
+      ? RecursivePartial<T[P]>
+      : T[P];
 };
 
 export type Mutable<T> = {
